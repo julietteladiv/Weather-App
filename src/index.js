6bd4c2793f6b2e73c66c23773b800f69b72a13c1
julietteladiv <juliettevidal01@gmail.com>
2025-04-28 16:18:08 +0100

    API added
    don't forget to refresh the page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+function refreshWeather(response) {
+  let newInput = document.querySelector("#temperature");
+  let newTemperature = response.data.temperature.current;
+  let newCity = document.querySelector("#city");
+
+  newCity.innerHTML = response.data.city;
+  newInput.innerHTML = Math.round(newTemperature);
+}
+
+function searchCity(city) {
+  let apiKey = "625t01634a276d07f8dc7437o8d4abf9";
+  let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(url).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let inputSearch = document.querySelector("#search-form-input");
   newCity = document.querySelector("#city");
-  newCity.innerHTML = inputSearch.value;
+  searchCity(inputSearch.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
