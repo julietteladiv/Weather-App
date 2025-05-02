e6db9a715e4511c92da1ba5999d0e6c6b8b95fd3
julietteladiv <juliettevidal01@gmail.com>
2025-05-02 10:31:05 +0100

    add icon in JS and change CSS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+function refreshWeather(response) {
+  let newInput = document.querySelector("#temperature");
+  let newTemperature = response.data.temperature.current;
+  let newCity = document.querySelector("#city");
+  let newDecritpion = document.querySelector("#description");
+  let newHumidity = document.querySelector("#Humidity");
+  let newWind = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let newEmoji = document.querySelector("#emoji");
+  console.log(response.data);
+  newEmoji.innerHTML = `<img src="${response.data.condition.icon_url}" class="emoji" />`;
+  newCity.innerHTML = response.data.city;
+  newInput.innerHTML = Math.round(newTemperature);
+  newDecritpion.innerHTML = response.data.condition.description;
+  newHumidity.innerHTML = response.data.temperature.humidity;
+  newWind.innerHTML = response.data.wind.speed;
+  timeElement.innerHTML = formatDate(date);
+}
 function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
@@ -18,25 +37,6 @@ function formatDate(date) {
 
   return `${day} ${hours}:${minutes}`;
 }
-
-function refreshWeather(response) {
-  let newInput = document.querySelector("#temperature");
-  let newTemperature = response.data.temperature.current;
-  let newCity = document.querySelector("#city");
-  let newDecritpion = document.querySelector("#description");
-  let newHumidity = document.querySelector("#Humidity");
-  let newWind = document.querySelector("#wind");
-  let timeElement = document.querySelector("#time");
-  let date = new Date(response.data.time * 1000);
-
-  newCity.innerHTML = response.data.city;
-  newInput.innerHTML = Math.round(newTemperature);
-  newDecritpion.innerHTML = response.data.condition.description;
-  newHumidity.innerHTML = response.data.temperature.humidity;
-  newWind.innerHTML = response.data.wind.speed;
-  timeElement.innerHTML = formatDate(date);
-}
-
 function searchCity(city) {
   let apiKey = "625t01634a276d07f8dc7437o8d4abf9";
   let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
